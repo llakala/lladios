3ea1ed06d21bb8f2c7e7a17e926f70ae70dda75b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-05-31 09:14:43 -0400

    adios.lib.merge.attrs.flat: take mutators at the very end

--- adios/lib/merge-attrs-flat.nix
+++ adios/lib/merge-attrs-flat.nix
@@ -1,4 +1,3 @@
-{ mutators }:
 let
   inherit (builtins)
     attrNames
@@ -13,49 +12,54 @@ let
 
   printList = list: "[${concatStringsSep ", " list}]";
 
-  checkPreviousMutators =
-    prevNames: name:
-    foldl' (
-      data: prevName:
-      let
-        intersection = intersectAttrs mutators.${name} mutators.${prevName};
-      in
-      if intersection != { } && data == null then
-        {
-          name = prevName;
-          sharedKeys = attrNames intersection;
-        }
-      else
-        data
-    ) null prevNames;
-
   # If we detect a name collision, we run more expensive logic to get
   # metadata on which mutators collided, and on which keys. If there's no
   # collisions, this is never evaluated.
-  errorMessage = foldl' (
-    prevNames: name:
+  makeErrorMessage =
+    mutators:
     let
-      # Each mutator checks whether it has collisions with the previous
-      # mutators.
-      collisionData = checkPreviousMutators prevNames name;
+      checkPreviousMutators =
+        prevNames: name:
+        foldl' (
+          data: prevName:
+          let
+            intersection = intersectAttrs mutators.${name} mutators.${prevName};
+          in
+          if intersection != { } && data == null then
+            {
+              name = prevName;
+              sharedKeys = attrNames intersection;
+            }
+          else
+            data
+        ) null prevNames;
+
     in
-    if collisionData == null then
-      prevNames ++ [ name ]
-    else if length collisionData.sharedKeys == 1 then
-      throw ''
-        Collision between mutators '${collisionData.name}' and '${name}' on key '${head collisionData.sharedKeys}'.
-      ''
-    else
-      throw ''
-        Collision between mutators '${collisionData.name}' and '${name}' on keys ${printList collisionData.sharedKeys}.
-      ''
-  ) [ ] (attrNames mutators);
+    foldl' (
+      prevNames: name:
+      let
+        # Each mutator checks whether it has collisions with the previous
+        # mutators.
+        collisionData = checkPreviousMutators prevNames name;
+      in
+      if collisionData == null then
+        prevNames ++ [ name ]
+      else if length collisionData.sharedKeys == 1 then
+        throw ''
+          Collision between mutators '${collisionData.name}' and '${name}' on key '${head collisionData.sharedKeys}'.
+        ''
+      else
+        throw ''
+          Collision between mutators '${collisionData.name}' and '${name}' on keys ${printList collisionData.sharedKeys}.
+        ''
+    ) [ ] (attrNames mutators);
 in
+{ mutators }:
 zipAttrsWith (
-  _name: values:
+  _: values:
   if length values == 1 then
     # Only one mutator
     head values
   else
-    errorMessage
+    makeErrorMessage mutators
 ) (attrValues mutators)
