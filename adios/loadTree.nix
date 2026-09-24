c38deea580fb885bf59859ad5e782e68d858cac0
alfarel <alfarelcynthesis@proton.me>
2026-09-24 12:52:44 -0400

    adios: warn on missing mutation

--- adios/loadTree.nix
+++ adios/loadTree.nix
@@ -202,8 +202,6 @@ let
         # it needs access to the newest version of args, not the cached one
         args' = if self.path == mutatorPath then args else resolution.args;
       in
-      # TODO: decide whether to error here, if a module didn't
-      # mutate when it was supposed to
       if resolution ? mutations.${self.path}.${name} then
         [
           (addErrorContext
@@ -212,7 +210,9 @@ let
           )
         ]
       else
-        [ ]
+        warn ''
+          '${self.path}': mutator '${resolution.path}' of option '${name}' was expected to provide a mutation. hint: did you make a typo?
+        '' [ ]
     ) option.mutators
     # If the mutators list is nonempty, have the value passed in eval/impl
     # stage go through the mergeFunc, under the current module's name.
