a43a5047b98b31e08291b034c8c3cd0923d3a4d0
Eman Resu <elevenaka11@gmail.com>
2026-09-24 15:37:04 -0400

    adios: small formatting cleanups

--- adios/loadTree.nix
+++ adios/loadTree.nix
@@ -210,9 +210,9 @@ let
           )
         ]
       else
-        warn ''
-          '${self.path}': mutator '${resolution.path}' of option '${name}' was expected to provide a mutation. hint: did you make a typo?
-        '' [ ]
+        warn
+          "'${self.path}': mutator '${resolution.path}' of option '${name}' was expected to provide a mutation. hint: did you make a typo?"
+          [ ]
     ) option.mutators
     # If the mutators list is nonempty, have the value passed in eval/impl
     # stage go through the mergeFunc, under the current module's name.
@@ -367,8 +367,9 @@ let
         );
         path = if path == "" then "/" else path;
 
-        ${if def ? types then "types" else null} =
-          addErrorContext "${errorPrefix}: in attribute 'types'" (checkTypedefs def.types);
+        ${if def ? types then "types" else null} = addErrorContext "${errorPrefix}: in attribute 'types'" (
+          checkTypedefs def.types
+        );
         ${if def ? mutations then "mutations" else null} =
           checkMutations "${errorPrefix}: in attribute 'mutations'" def.mutations;
         ${if def ? lib then "lib" else null} = addErrorContext "${errorPrefix}: in attribute 'lib'" (
